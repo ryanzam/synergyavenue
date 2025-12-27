15e1893c7edfe2f63f0f0ac67b635ff123f6abb9
ᖇᗩᑎᒎᗩᗰ <ryanzam2005@yahoo.com>
2025-12-27 12:19:05 +0545

    add login functionality

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,16 @@
 import "dotenv/config";
-import { PrismaClient } from '../generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient()
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+};
 
-export { prisma }
+export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        log: ["query"], // Optional: for debugging
+    });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
